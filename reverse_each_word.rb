c8cf5dcfa20e06ff9a6fa698aad8d6e509e7c121
Jessica Michael <pantherjmm@yahoo.com>
2019-11-15 08:43:22 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,12 @@
+'''ruby
+
+sentence1 = "I am like hey whats up hello"
+reverse_each_word (sentence1)
+
+sentence2 = "I miss vine"
+reverse_each_word(sentence2)
+
+big_dogs = ["german_shepherd", "great_dane", "mastif"]
+big_dogs.each do |big_dogs|
+  puts "This big_dogs wants to kiss you!
+end
